1cc87395062b64d0e1404abebd1eac30c8fcc260
bensolo-io <benji.lilley@solo.io>
2023-06-28 19:04:28 -0400

    0.0.0.0/0

--- terraform/_submodules/vpc/vpc.tf
+++ terraform/_submodules/vpc/vpc.tf
@@ -133,7 +133,7 @@ resource "aws_security_group" "common" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/8", "192.168.0.0/16"]
+    cidr_blocks = ["10.0.0.0/8", "192.168.0.0/16", "0.0.0.0/0"]
   }
 
   egress {
@@ -141,7 +141,7 @@ resource "aws_security_group" "common" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/8", "192.168.0.0/16"]
+    cidr_blocks = ["10.0.0.0/8", "192.168.0.0/16", "0.0.0.0/0"]
   }
 
   egress {
