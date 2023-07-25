9fbb116e99b90aba8dfdf1cacc9cca09958f136c
bdlilley <benji.lilley@solo.io>
2023-07-25 08:47:25 -0400

    fix sizes

--- terraform/_submodules/eks-simple/eks-nodes.tf
+++ terraform/_submodules/eks-simple/eks-nodes.tf
@@ -14,9 +14,9 @@ module "eks_managed_node_group" {
   cluster_primary_security_group_id = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
   vpc_security_group_ids            = concat(var.cluster.securityGroupIds, [each.value.commonClusterSg])
 
-  min_size     = 1
-  max_size     = 2
-  desired_size = 1
+  min_size     = try(each.value.min_size, 0)
+  max_size     = try(each.value.max_size, 4)
+  desired_size = try(each.value.desired_size, 1)
 
   key_name = try(var.cluster.keyName, "")
 
