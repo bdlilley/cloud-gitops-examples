00ce0f3026fd26cf1eb9fb7309a92869aefedd5a
bdlilley <benji.lilley@solo.io>
2023-08-02 14:49:37 -0400

    wip

--- examples/_scripts/gen-argocd.sh
+++ examples/_scripts/gen-argocd.sh
@@ -4,6 +4,6 @@ rm -rf .dist
 mkdir -p .dist/argocd-install
 mkdir -p .dist/argocd-apps
 
-gomplate -d tf=.module-outputs.json -f ../_gomplates/argocd-apps.tmpl > .dist/argocd-apps/apps.yaml
-gomplate -d tf=.module-outputs.json -f ../_gomplates/argocd-install.tmpl > .dist/argocd-install/kustomization.yaml
+gomplate -d env=_env.yaml -d tf=.module-outputs.json -f ../_gomplates/argocd-apps.tmpl > .dist/argocd-apps/apps.yaml
+gomplate -d env=_env.yaml -d tf=.module-outputs.json -f ../_gomplates/argocd-install.tmpl > .dist/argocd-install/kustomization.yaml
 # gomplate -d tf=.module-outputs.json -f ../_gomplates/gp-install.tmpl > .dist/gp-install/kustomization.yaml
