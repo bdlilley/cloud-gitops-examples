e2a2591094ef1301636e2ac427459132e3b94987
bdlilley <benji.lilley@solo.io>
2023-08-02 17:10:47 -0400

    restructure

--- examples/_scripts/gen-argocd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf .dist
-mkdir -p .dist/argocd-install
-mkdir -p .dist/argocd-apps
-
-gomplate -d env=_env.yaml -d tf=.module-outputs.json -f ../_gomplates/argocd-apps.tmpl > .dist/argocd-apps/apps.yaml
-gomplate -d env=_env.yaml -d tf=.module-outputs.json -f ../_gomplates/argocd-install.tmpl > .dist/argocd-install/kustomization.yaml
-# gomplate -d tf=.module-outputs.json -f ../_gomplates/gp-install.tmpl > .dist/gp-install/kustomization.yaml
