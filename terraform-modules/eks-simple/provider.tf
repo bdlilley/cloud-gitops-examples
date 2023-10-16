9544bbcf473b985a0d3f3d27563032b7194d4d5c
bdlilley <benji.lilley@solo.io>
2023-10-16 09:09:51 -0400

    dont specify provider

--- terraform-modules/eks-simple/provider.tf
+++ terraform-modules/eks-simple/provider.tf
@@ -1,13 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      # explicit dependency versions prevent breakage from new releases 
-      version = "4.52.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       # explicit dependency versions prevent breakage from new releases 
+#       version = "4.52.0"
+#     }
+#   }
+# }
 
-data "aws_region" "current" {}
-
-data "aws_caller_identity" "current" {}
--- terraform-modules/vpc-simple/provider.tf
+++ terraform-modules/vpc-simple/provider.tf
@@ -1,9 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      # explicit dependency versions prevent breakage from new releases 
-      version = "4.52.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       # explicit dependency versions prevent breakage from new releases 
+#       version = "4.52.0"
+#     }
+#   }
+# }
